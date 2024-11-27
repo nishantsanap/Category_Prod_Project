aa824f2e9cfd287d2a932cf17ecbf20a566e7532
Nishant Sanap <144839538+nishantsanap@users.noreply.github.com>
2024-11-27 18:06:38 +0530

    Update CategoryProdAppApplication.java

--- src/main/java/com/ty/CategoryProdAppApplication.java
+++ src/main/java/com/ty/CategoryProdAppApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CategoryProdAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CategoryProdAppApplication.class, args); 
-		
+		SpringApplication.run(CategoryProdAppApplication.class, args); 	
 	}
 
 }
